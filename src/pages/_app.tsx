46a8c54ed6a2209677b784602998009e5e63e388
sao-coding <jeter.nice@gmail.com>
2023-03-18 23:23:40 +0800

    improve ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import '@/style.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
+import { Session } from 'next-auth'
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps) {
+}: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
