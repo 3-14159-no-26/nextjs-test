89f83ac114ea7fdf55783aff5e858957e80f2b2c
sao-coding <jeter.nice@gmail.com>
2023-05-01 02:41:54 +0800

    修復 時區錯誤

--- src/pages/voice/index.tsx
+++ src/pages/voice/index.tsx
@@ -1,4 +1,5 @@
 import { format } from 'date-fns'
+import { utcToZonedTime } from 'date-fns-tz'
 import React, { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 
@@ -18,22 +19,34 @@ const Voice = () => {
 
     if (isLoading) return <div>Loading...</div>
     if (error) return <div>Error fetching data</div>
+
     return (
         <div className="layout">
             <div className="message">
-                {data.map((item) => (
-                    <div key={item.id}>
-                        <div className="content">
-                            <div className="">{item.message}</div>
-                            <div className="">
-                                {format(
-                                    new Date(item.time),
-                                    'yyyy-MM-dd HH:mm:ss'
-                                )}
+                {data.map((item) => {
+                    const utcTime = new Date(item.time)
+                    // console.log(utcTime)
+                    //
+                    const taipeiTime = utcToZonedTime(
+                        utcTime,
+                        'America/Scoresbysund'
+                    )
+                    console.log(taipeiTime)
+
+                    const formattedTime = format(
+                        taipeiTime,
+                        'yyyy-MM-dd HH:mm:ss'
+                    )
+
+                    return (
+                        <div key={item.id}>
+                            <div className="content">
+                                <div className="">{item.message}</div>
+                                <div className="">{formattedTime}</div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    )
+                })}
             </div>
         </div>
     )
