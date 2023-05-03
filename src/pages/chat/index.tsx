d23d6dca5edd75a8e6080117a6919d7cd0e32abc
sao-coding <jeter.nice@gmail.com>
2023-05-03 23:58:09 +0800

    新增 語音輸入輸出

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,29 +1,75 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/react'
 import NavBar from '@/components/NavBar'
 
 const Chat = () => {
-    const [input, setInput] = useState('')
+    const [value, setValue] = useState('')
     const [loading, setLoading] = useState(false)
     const [messages, setMessages] = useState([])
     const [changes, setChanges] = useState(1)
 
+    const inputRef = useRef(null)
+    useEffect(() => {
+        console.log(
+            'webkitSpeechRecognition',
+            'webkitSpeechRecognition' in window
+        )
+
+        console.log('SpeechRecognition', 'SpeechRecognition' in window)
+
+        if (!('webkitSpeechRecognition' in window)) {
+            alert('您的瀏覽器不支持語音輸入')
+            return
+        }
+    }, [])
     const send = async () => {
-        console.log('input', input)
+        console.log('input', inputRef.current.value)
         setLoading(true)
         const data = await fetch('/api/openai', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ content: input }),
+            body: JSON.stringify({ content: inputRef.current.value }),
         })
         const res = await data.json()
-        // console.log('res', res)
+        voiceOutput(res.content)
         setLoading(false)
-        setMessages([...messages, input, res.content])
-        setInput('')
+        setMessages([...messages, inputRef.current.value, res.content])
+        inputRef.current.value = ''
+    }
+
+    const voiceInput = () => {
+        console.log('voiceInput')
+
+        if (!('webkitSpeechRecognition' in window)) {
+            alert('您的瀏覽器不支持語音輸入')
+            return
+        }
+        const recognition = new window.webkitSpeechRecognition()
+
+        // 設置語言為中文
+        recognition.lang = 'zh-TW'
+
+        // 設置連續語音辨識
+        // recognition.continuous = true
+
+        // 設置語音辨識事件處理器
+        recognition.onresult = (event) => {
+            const transcript = event.results[0][0].transcript
+            console.log('輸入的文本：', transcript)
+            setValue(transcript)
+            inputRef.current.value = transcript
+        }
+
+        recognition.start()
+    }
+
+    const voiceOutput = (content) => {
+        const msg = new SpeechSynthesisUtterance(content)
+        msg.lang = 'zh-TW'
+        window.speechSynthesis.speak(msg)
     }
 
     const handleKeyDown = (event) => {
@@ -50,15 +96,10 @@ const Chat = () => {
                     </div>
                     <div className="h-96 bg-white rounded-md mb-5 overflow-y-auto">
                         {messages.map((message, index) => (
-                            <>
-                                <div key={index} className="p-2 border-b">
-                                    {message}
-                                </div>
-                            </>
+                            <div key={index} className="p-2 border-b">
+                                {message}
+                            </div>
                         ))}
-                        {input && (
-                            <div className="p-2 border-b input">{input}</div>
-                        )}
                         {loading && (
                             <div className="p-2 color-gray-400">Loading...</div>
                         )}
@@ -67,8 +108,7 @@ const Chat = () => {
                         <textarea
                             className="w-4/5 p-2 border rounded-md h-full"
                             rows={changes}
-                            value={input}
-                            onChange={(e) => setInput(e.target.value)}
+                            ref={inputRef}
                             onKeyDown={handleKeyDown}
                         />
                         <button
@@ -77,10 +117,19 @@ const Chat = () => {
                         >
                             傳送
                         </button>
+                        {/* 語音輸入 */}
+                        <button
+                            className="text-white bg-sky-700 p-2 rounded-md hover:bg-sky-500 w-1/5 h-full"
+                            onClick={voiceInput}
+                        >
+                            語音
+                        </button>
+
                         <div className="absolute bottom-0 right-1 text-xs text-gray-400">
                             Powered by OpenAI & 唯一
                         </div>
                     </div>
+                    <p>語音辨識 {value}</p>
                 </div>
             </div>
         </>
