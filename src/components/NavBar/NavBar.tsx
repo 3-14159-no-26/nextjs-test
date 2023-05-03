7662e32cd447e41220b7e04a19b7ad3d05aa9f7a
sao-coding <jeter.nice@gmail.com>
2023-05-03 12:32:13 +0800

    修該 導覽列文字

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,18 +4,18 @@ const NavBar = () => {
         <div className="flex justify-between items-center p-5 bg-slate-500">
             <div className="flex items-center space-x-2">
                 <Link href="/">
-                    <div className="text-white">Home</div>
+                    <div className="text-white">首頁</div>
                 </Link>
                 <Link href="/chat">
-                    <div className="text-white">Chat</div>
+                    <div className="text-white">聊天</div>
                 </Link>
                 <Link href="/voice">
-                    <div className="text-white">Voice</div>
+                    <div className="text-white">語音</div>
                 </Link>
             </div>
             <div className="flex items-center space-x-2">
                 <Link href="/api/auth/signout">
-                    <div className="text-white">Sign out</div>
+                    <div className="text-white">登出</div>
                 </Link>
             </div>
         </div>
