c2d120d053b91043f0584c9d8d2690c11c03d437
sao-coding <jeter.nice@gmail.com>
2023-05-03 12:35:17 +0800

    修復 網頁語言

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html lang="zh-Hant-TW">
+        <Html lang="zh-Hans-TW">
             <Head />
             <body>
                 <Main />
